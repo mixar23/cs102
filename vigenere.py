6807478e3e3d543e1d5a116e23ed2c4eb6fc6f45
mixar23 <54994783+mixar23@users.noreply.github.com>
2019-10-10 21:14:53 +0300

    Add files via upload

---
+++ vigenere.py
@@ -0,0 +1,47 @@
+def encrypt_vigenere(plaintext:str,keyword:str) -> str:
+     a = ('abcdefghijklmnopqrstuvwxyz')
+     b =('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+     ciphertext=str()
+     s = 0
+     for l in plaintext:
+         k = 0
+         for n in range(26):
+             if l == a[n]:
+                 s+=1
+                 ciphertext += str(a[(a.index(l)+a.index(keyword[s%len(keyword)]))%26])
+             elif l == b[n]:
+                 s+=1
+                 ciphertext += str(b[(b.index(l)+b.index(keyword[s%len(keyword)]))%26])
+             else:
+                    k += 1
+         if k == 26:
+             ciphertext+=(' ')
+     return(ciphertext)
+def decrypt_vigenere(ciphertext:str,keyword:str) -> str:
+     a = ('abcdefghijklmnopqrstuvwxyz')
+     b =('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+     plaintex=str()
+     s = 0
+     for l in ciphertext:
+         k = 0
+         for n in range(26):
+             if l == a[n]:
+                 s+=1
+                 plaintex += str(a[(a.index(l)-a.index(keyword[s%len(keyword)]))%26])
+             elif l == b[n]:
+                 s+=1
+                 plaintex += str(b[(b.index(l)-b.index(keyword[s%len(keyword)]))%26])
+             else:
+                    k += 1
+         if k == 26:
+             plaintex+=(' ')
+     return(plaintex)     
+x = input('Enter text: ')
+y = input('Enter keyword: ')
+g = input('Do you want do encrypt this text?(yes/no):')
+if g == ('yes'):
+    print(encrypt_vigenere (x,y))
+elif g ==('no'):
+    print(decrypt_vigenere(x,y))
+else:
+    print('incorret input')
