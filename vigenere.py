b747bfa4d07c38c5f2538668c64730d501607665
mixar23 <54994783+mixar23@users.noreply.github.com>
2019-10-12 18:19:04 +0300

    Update vigenere.py

--- vigenere.py
+++ vigenere.py
@@ -1,54 +1,59 @@
-def encrypt_vigenere(plaintext:str,keyword:str) -> str:
-      """
-    >>> encrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    >>> encrypt_vigenere("python", "a")
-    'python'
-    >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
-    'LXFOPVEFRNHR'
+def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
-     a = ('abcdefghijklmnopqrstuvwxyz')
-     b =('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-     ciphertext=str()
-     s = 0
-     for l in plaintext:
-         k = 0
-         for n in range(26):
-             if l == a[n]:
-                 s+=1
-                 ciphertext += str(a[(a.index(l)+a.index(keyword[s%len(keyword)]))%26])
-             elif l == b[n]:
-                 s+=1
-                 ciphertext += str(b[(b.index(l)+b.index(keyword[s%len(keyword)]))%26])
-             else:
-                    k += 1
-         if k == 26:
-             ciphertext+=(' ')
-     return(ciphertext)
-def decrypt_vigenere(ciphertext:str,keyword:str) -> str:
-      """
-    >>> decrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    >>> decrypt_vigenere("python", "a")
-    'python'
-    >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
-    'ATTACKATDAWN'
+  >>> encrypt_vigenere("PYTHON", "A")
+  'PYTHON'
+  >>> encrypt_vigenere("python", "a")
+  'python'
+  >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
+  'LXFOPVEFRNHR'
+  """
+
+
+a = ('abcdefghijklmnopqrstuvwxyz')
+b = ('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+ciphertext = str()
+s = 0
+for l in plaintext:
+    k = 0
+    for n in range(26):
+        if l == a[n]:
+            s += 1
+            ciphertext += str(a[(a.index(l) + a.index(keyword[s % len(keyword)])) % 26])
+        elif l == b[n]:
+            s += 1
+            ciphertext += str(b[(b.index(l) + b.index(keyword[s % len(keyword)])) % 26])
+        else:
+            k += 1
+    if k == 26:
+        ciphertext += (' ')
+return (ciphertext)
+
+
+def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     """
-     a = ('abcdefghijklmnopqrstuvwxyz')
-     b =('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-     plaintex=str()
-     s = 0
-     for l in ciphertext:
-         k = 0
-         for n in range(26):
-             if l == a[n]:
-                 s+=1
-                 plaintex += str(a[(a.index(l)-a.index(keyword[s%len(keyword)]))%26])
-             elif l == b[n]:
-                 s+=1
-                 plaintex += str(b[(b.index(l)-b.index(keyword[s%len(keyword)]))%26])
-             else:
-                    k += 1
-         if k == 26:
-             plaintex+=(' ')
-     return(plaintex)     
+  >>> decrypt_vigenere("PYTHON", "A")
+  'PYTHON'
+  >>> decrypt_vigenere("python", "a")
+  'python'
+  >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
+  'ATTACKATDAWN'
+  """
+
+    a = ('abcdefghijklmnopqrstuvwxyz')
+    b = ('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    plaintex = str()
+    s = 0
+    for l in ciphertext:
+        k = 0
+        for n in range(26):
+            if l == a[n]:
+                s += 1
+                plaintex += str(a[(a.index(l) - a.index(keyword[s % len(keyword)])) % 26])
+            elif l == b[n]:
+                s += 1
+                plaintex += str(b[(b.index(l) - b.index(keyword[s % len(keyword)])) % 26])
+            else:
+                k += 1
+        if k == 26:
+            plaintex += (' ')
+    return (plaintex)
