0377bc75cfb990d23d9c58119f7db0d2a1b6c406
mixar23 <54994783+mixar23@users.noreply.github.com>
2019-10-12 18:21:40 +0300

    Update vigenere.py

--- vigenere.py
+++ vigenere.py
@@ -8,25 +8,24 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
   'LXFOPVEFRNHR'
   """
 
-
-a = ('abcdefghijklmnopqrstuvwxyz')
-b = ('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-ciphertext = str()
-s = 0
-for l in plaintext:
-    k = 0
-    for n in range(26):
-        if l == a[n]:
-            s += 1
-            ciphertext += str(a[(a.index(l) + a.index(keyword[s % len(keyword)])) % 26])
-        elif l == b[n]:
-            s += 1
-            ciphertext += str(b[(b.index(l) + b.index(keyword[s % len(keyword)])) % 26])
-        else:
-            k += 1
-    if k == 26:
-        ciphertext += (' ')
-return (ciphertext)
+    a = ('abcdefghijklmnopqrstuvwxyz')
+    b = ('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    ciphertext = str()
+    s = 0
+    for l in plaintext:
+        k = 0
+        for n in range(26):
+            if l == a[n]:
+                s += 1
+                ciphertext += str(a[(a.index(l) + a.index(keyword[s % len(keyword)])) % 26])
+            elif l == b[n]:
+                s += 1
+                ciphertext += str(b[(b.index(l) + b.index(keyword[s % len(keyword)])) % 26])
+            else:
+                k += 1
+        if k == 26:
+            ciphertext += (' ')
+    return (ciphertext)
 
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
