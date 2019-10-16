4170cdea6e341e896e5d90f3375c4099312b9498
mixar23 <54994783+mixar23@users.noreply.github.com>
2019-10-16 20:06:35 +0300

    Update vigenere.py

--- vigenere.py
+++ vigenere.py
@@ -1,58 +1,63 @@
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
-  >>> encrypt_vigenere("PYTHON", "A")
-  'PYTHON'
-  >>> encrypt_vigenere("python", "a")
-  'python'
-  >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
-  'LXFOPVEFRNHR'
-  """
-
-    a = ('abcdefghijklmnopqrstuvwxyz')
-    b = ('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    >>> encrypt_vigenere("PYTHON", "A")
+    'PYTHON'
+    >>> encrypt_vigenere("python", "a")
+    'python'
+    >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
+    'LXFOPVEFRNHR'
+    """
+    while len(plaintext) > len(keyword):
+        keyword += keyword
     ciphertext = str()
-    s = 0
-    for l in plaintext:
-        k = 0
-        for n in range(26):
-            if l == a[n]:
-                s += 1
-                ciphertext += str(a[(a.index(l) + a.index(keyword[s % len(keyword)])) % 26])
-            elif l == b[n]:
-                s += 1
-                ciphertext += str(b[(b.index(l) + b.index(keyword[s % len(keyword)])) % 26])
+    if keyword.isupper():
+        for i in range(len(plaintext)):
+            n = ord(plaintext[i])
+            x = ord(keyword[i])
+            if n + (x % 65) > 90:
+                ciphertext += chr(n + (x % 65) - 26)
             else:
-                k += 1
-        if k == 26:
-            ciphertext += (' ')
-    return (ciphertext)
+                ciphertext += chr(n + (x % 65))
+    else:
+        for i in range(len(plaintext)):
+            n = ord(plaintext[i])
+            x = ord(keyword[i])
+            if (n + (x % 97)) > 122:
+                ciphertext += chr(n + (x % 97) - 26)
+            else:
+                ciphertext += chr(n + (x % 97))
+
+    return ciphertext
 
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     """
-  >>> decrypt_vigenere("PYTHON", "A")
-  'PYTHON'
-  >>> decrypt_vigenere("python", "a")
-  'python'
-  >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
-  'ATTACKATDAWN'
-  """
-
-    a = ('abcdefghijklmnopqrstuvwxyz')
-    b = ('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-    plaintex = str()
-    s = 0
-    for l in ciphertext:
-        k = 0
-        for n in range(26):
-            if l == a[n]:
-                s += 1
-                plaintex += str(a[(a.index(l) - a.index(keyword[s % len(keyword)])) % 26])
-            elif l == b[n]:
-                s += 1
-                plaintex += str(b[(b.index(l) - b.index(keyword[s % len(keyword)])) % 26])
+    >>> decrypt_vigenere("PYTHON", "A")
+    'PYTHON'
+    >>> decrypt_vigenere("python", "a")
+    'python'
+    >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
+    'ATTACKATDAWN'
+    """
+    while len(ciphertext) > len(keyword):
+        keyword += keyword
+    plaintext = str()
+    if keyword.isupper():
+        for i in range(len(ciphertext)):
+            n = ord(ciphertext[i])
+            x = ord(keyword[i])
+            print(n, x)
+            if n >= x:
+                plaintext += chr(n - (x % 65))
             else:
-                k += 1
-        if k == 26:
-            plaintex += (' ')
-    return (plaintex)
+                plaintext += chr(n + 26 - (x % 65))
+    else:
+        for i in range(len(ciphertext)):
+            n = ord(ciphertext[i])
+            x = ord(keyword[i])
+            if n >= x:
+                plaintext += chr(n - (x % 97))
+            else:
+                plaintext += chr(n + 26 - (x % 97))
+
+    return (plaintext)
