1683933ded42fd91b449194a6730a8e653194c5d
mixar23 <54994783+mixar23@users.noreply.github.com>
2019-12-21 16:43:03 +0300

    Update networks.py

--- homework04/networks.py
+++ homework04/networks.py
@@ -1,78 +1,58 @@
 from api import get_friends
-import igraph
 import time
-
-from igraph import Graph, plot
-from typing import Union, List, Tuple
+import igraph
 
 
-users_ids = []
-name_list = []
-friends = get_friends(228147479)
-for friend in friends:
-    try:
-        id = friend['id']
-        name = friend['first_name'] + ' ' + friend['last_name']
-        users_ids.append(id)
-        name_list.append(name)
-    except ValueError:
-        pass
 
-def get_network(users_ids, as_edgelist=True) -> Union[List[List[int]], List[Tuple[int, int]]]:
+
+def get_network(user_id: int, as_edgelist) -> list:
+    users_ids = get_friends(user_id)['response']['items']
+    edges = []
+    matrix = [[0] * num for i in range(num)]
+
+    for user1 in range(num):
+        friends = get_friends(users_ids[user1])['response']['items']
+        for user2 in range(user1 + 1, num):
+            if users_ids[user2] in friends:
+                if as_edgelist:
+                    edges.append((user1, user2))
+                else:
+                    matrix[user1][user2] = 1
+                    matrix[user2][user1] = 1
+        time.sleep(0.33333334)
+
     if as_edgelist:
-        edge_list = []
-        for user in users_ids:
-            try:
-                friend_list = get_friends(user)
-            except KeyError:
-                continue
-            else:
-                for friend in friend_list:
-                    if friend['id'] in users_ids:
-                        edge_list.append((users_ids.index(user), users_ids.index(friend['id'])))
-        return edge_list
-    else:
-        matrix = [[0] * len(users_ids) for _ in range(len(users_ids))]
-        for user in users_ids:
-            try:
-                friend_list = get_friends(user)
-            except KeyError:
-                continue
-            else:
-                for friend in friend_list:
-                    if friend['id'] in users_ids:
-                        matrix[users_ids.index(user)][users_ids.index(friend['id'])] = 1
-        return matrix
+        return edges
+    return matrix
 
 
-def plot_graph(edge_list: list, name_list=[]) -> None:
-    if name_list == 0:
-        vertices = [i for i in range(len(edge_list))]
-    else:
-        vertices = name_list
-    g = Graph(vertex_attrs={"label": vertices},
-              edges=edge_list, directed=False)
-    g.simplify(multiple=True, loops=True)
-    N = len(vertices)
+def plot_graph(user_id: int) -> None:
+    surnames = get_friends(user_id, 'last_name')['response']['items']
+    vertices = [i['last_name'] for i in surnames]
+    edges = get_network(user_id, True)
+
+    g = igraph.Graph(vertex_attrs={"shape": "circle",
+                                   "label": vertices,
+                                   "size": 10},
+                     edges=edges, directed=False)
+
+    n = len(vertices)
     visual_style = {
-        "vertex_size": 20,
-        "bbox": (2000, 2000),
-        "margin": 100,
         "vertex_label_dist": 1.6,
+        "vertex_size": 20,
         "edge_color": "gray",
-        "autocurve": True,
         "layout": g.layout_fruchterman_reingold(
             maxiter=100000,
-            area=N ** 2,
-            repulserad=N ** 2)
+            area=n ** 2,
+            repulserad=n ** 2)
     }
-
+    g.simplify(multiple=True, loops=True)
     clusters = g.community_multilevel()
     pal = igraph.drawing.colors.ClusterColoringPalette(len(clusters))
     g.vs['color'] = pal.get_many(clusters.membership)
-    plot(g, **visual_style)
+    igraph.plot(g, **visual_style)
 
-
-# Вызов функций для проверки:
-# plot_graph(get_network(users_ids), name_list)
-# print(get_network(users_ids, False))
+if __name__ == '__main__':
+    user_id = int(input('Enter id: '))
+    num = get_friends(user_id)['response']['count']
+    plot_graph(user_id)
